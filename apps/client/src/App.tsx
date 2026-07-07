cbfe7cb5102e7b78214e96fa7d4d4adfb3289adb
kofi arhin <kofiarhin69@gmail.com>
2026-07-08 00:31:53 +0100

    fix: bug

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -4,7 +4,7 @@ import { AppRouter } from "./app/router";
 import { ProgressProvider } from "./app/ProgressProvider";
 
 export const App = () => (
-  <BrowserRouter>
+  <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
     <ProgressProvider>
       <AppRouter />
     </ProgressProvider>
