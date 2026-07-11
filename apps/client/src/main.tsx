75f2de97221fd3fcd23039ecea9917b57f291975
Kofi Arhin <kofiarhin69@gmail.com>
2026-07-11 21:13:52 +0100

    style: load deep purple piano highlight theme

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./piano-highlight.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
