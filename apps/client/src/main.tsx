63764c45b5a5c7b7f4904d83f17666d48c5a39b5
Kofi Arhin <kofiarhin69@gmail.com>
2026-07-14 13:43:13 +0100

    feat: load timeline player enhancement styles

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./piano-highlight.css";
+import "./timeline-player-enhancements.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
